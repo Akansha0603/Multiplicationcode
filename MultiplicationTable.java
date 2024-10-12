33ec356583e24f4a04fa86a5ac299e5e4e915ee6
Akansha0603 <akanshaverma950@gmail.com>
2024-10-12 20:00:20 +0530

    Update MultiplicationTable.java

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,22 +1,11 @@
-import java.util.Scanner;
-
 public class MultiplicationTable {
 
     public static void main(String[] args) {
-        // Create a Scanner object to read input
-        Scanner scanner = new Scanner(System.in);
+        int number = 5;  // Hardcoded number for the multiplication table
 
-        // Ask the user to input a number
-        System.out.print("Enter a number to print its multiplication table: ");
-        int number = scanner.nextInt();
-
-        // Loop to print the multiplication table
         System.out.println("Multiplication Table of " + number + ":");
         for (int i = 1; i <= 10; i++) {
             System.out.println(number + " x " + i + " = " + (number * i));
         }
-
-        // Close the scanner
-        scanner.close();
     }
 }
