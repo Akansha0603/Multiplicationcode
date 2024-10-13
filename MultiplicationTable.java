087095ceffc3210d878dce7b2ce04cd270131b94
Akansha0603 <akanshaverma950@gmail.com>
2024-10-13 23:38:30 +0530

    Update MultiplicationTable.java

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,7 +1,7 @@
 public class MultiplicationTable {
 
     public static void main(String[] args) {
-        int number = 15;  // Hardcoded number for the multiplication table
+        int number = 215;  // Hardcoded number for the multiplication table
 
         System.out.println("Multiplication Table of " + number + ":");
         for (int i = 1; i <= 10; i++) {
